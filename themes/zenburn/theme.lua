1836e541d9920f5349a0cbc7daef27a5789c35fa
Felix Riedel <riedel@google.com>
2014-11-28 11:25:48 +0000

    vain layout icons

--- themes/zenburn/theme.lua
+++ themes/zenburn/theme.lua
@@ -105,6 +105,14 @@ theme.layout_max        = theme.confdir .. "/icons/layouts/max.png"
 theme.layout_fullscreen = theme.confdir .. "/icons/layouts/fullscreen.png"
 theme.layout_magnifier  = theme.confdir .. "/icons/layouts/magnifier.png"
 theme.layout_floating   = theme.confdir .. "/icons/layouts/floating.png"
+
+theme.layout_termfair      = os.getenv("HOME") .. "/.config/awesome/vain/themes/default/layouts/termfairw.png"
+theme.layout_browse        = os.getenv("HOME") .. "/.config/awesome/vain/themes/default/layouts/browsew.png"
+theme.layout_gimp          = os.getenv("HOME") .. "/.config/awesome/vain/themes/default/layouts/gimpw.png"
+theme.layout_cascade       = os.getenv("HOME") .. "/.config/awesome/vain/themes/default/layouts/cascadew.png"
+theme.layout_cascadebrowse = os.getenv("HOME") .. "/.config/awesome/vain/themes/default/layouts/cascadebrowsew.png"
+theme.layout_centerwork    = os.getenv("HOME") .. "/.config/awesome/vain/themes/default/layouts/centerworkw.png"
+
 -- }}}
 
 -- {{{ Widget icons
